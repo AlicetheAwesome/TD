cedd21130dd624d8cb1316b26a9d77157672e749
AlicetheAwesome <alicetingchen@gmail.com>
2016-01-09 12:38:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ var enemy = {
       this.x += this.direction.x * this.speed/FPS;
       this.y += this.direction.y * this.speed/FPS;
     }
-    if(isCollided == true){
+    if(isCollided === true){
       this.x = enemyPath[this.pathDes].x;
       this.y = enemyPath[this.pathDes].y;
       this.path.Des += 1;
