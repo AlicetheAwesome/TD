76110c8a6b4b1451c1067f3f25b3ad45b8816d30
AlicetheAwesome <alicetingchen@gmail.com>
2016-01-09 10:56:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ $("#game-canvas").mousemove(
   
 
 function draw(){
-  //enemy.move();
+  enemy.move();
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(heroImg, hero.x, hero.y);
   ctx.drawImage(towerImg, 0, 0, 64, 64 );
